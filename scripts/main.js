ee73ab5216fe284e89892a247e70400f02d188f9
Dimitri Walters <me@dimi.io>
2015-10-17 01:10:26 -0400

    Modify damage formula

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,17 @@
 (function() {
-    var attack, defense, base, modifier, damage;
+    var attack, defense, base, multiple, modifer, damage, statsNumber;
     $('.calc-button').click(function() {
-        var statsNumber = $(this).data("stats");
+        statsNumber = $(this).data("stats");
         attack = parseInt($('#attack' + statsNumber).val());
         defense = parseInt($('#defense' + statsNumber).val());
         base = parseInt($('#base' + statsNumber).val());
-        modifier = parseFloat($('#modifier' + statsNumber).val());
-        damage = (attack/defense) * base * modifier;
+        multiple = parseFloat($('#multiple' + statsNumber).val());
+        modifier = multiple;
+        if (randomit.generateNumber(1, 16) === 1) {
+            modifier *= 1.5;
+        }
+        modifier *= randomit.generateNumber(85, 100)/100;
+        damage = ((210/250) * (attack/defense) * base + 2) * modifier;
 
         $('#damage' + statsNumber).text(damage);
     });
